6138a4e05d55ecdfd373cdf7ac66b586ffc060f7
WhyK <windchimeyk@gmail.com>
2021-05-29 04:07:16 +0900

    remove unnecessary file and code

--- js/img.js
+++ js/img.js
@@ -1,16 +1,5 @@
-
-$('.phone').change(function () {
-    var val1 = $(this).val();
-    console.log('1つめ' + val1);
-
-    var selectNumId = '#' + val1;
-    // $('ul li').hide();
-    //選択したブロックのみを表示
-    $(selectNumId).show();
-});
-
 $(document).ready(function () {
-    $('.slider').slick({
-        autoplay: true
+  $(".slider").slick({
+    autoplay: true,
   });
 });
